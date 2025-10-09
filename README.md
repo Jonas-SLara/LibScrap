2fff2a5ebe5e43d91ffc665232b678c8e30b5cd8
Aluno <Aluno@politecnico.local>
2025-10-09 11:11:20 -0300

    inicialização de lista por categorias e web crawling

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# Inicialização do projeto com venv
 
+# Guia de Uso
+
+## Inicialização do projeto com venv caso não há
+
+LINUX UBUNTU
 ```bash
 pyhton3 -m venv venv
+```
+
+## ativação do ambiente virtual
+
+LINUX UBUNTU
+```bash
+source venv/bin/activate (linux)
+```
+
+WINDOWNS
+```bash
+.\meu_ambiente_virtual\Scripts\activate
 ```
--- main.py
+++ main.py
@@ -1,37 +1,87 @@
-# resoluçoes de exercicios em python estão nos imports aqui será testado
-from datetime import datetime
-import src.aula1 as aula1
-import src.aula2 as aula2
-import src.aula3 as aula3
+"""
+1. acessar a página principal
+2. encontrar e extrair links das categorias de livros que
+estão na página principal
+3. depois disso iterar sobre cada link de categoria e para
+para cada categoria fazer:
+    - acessar a página para cada categoria
+"""
+
+from random import random, sample
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
 import time
+import re
 
-def testeOrdenacao():
-    temp = aula2.gerar_lista(100000, 1, 100000)  
-    t = time.time()
-    aula2.quicksort(temp)
-    print("tempo de execução quicksort: %f" %(time.time() - t))
+listaURLs = ['https://books.toscrape.com/index.html']
+listaURLsVisitadas = []
+URL_BASE = 'https://books.toscrape.com/'
 
-lista = [
-    "joao.silva@gmail.com",    # válido
-    "ana_123@ufsm.br",         # válido
-    "teste-user@empresa.co.uk",# válido
-    "maria@site",              # inválido (sem TLD)
-    "fulano@.com",             # inválido (domínio começa com .)
-    ".joao@site.com",          # inválido (nome começa com ponto)
-]
+#função para encontrar os links de 5 categorias
+#que preciso, 2 deverão ter paginação que nada mais é
+#que um link com botão next na página
 
-#aula3.enderecoWeb(lista)
+def encontrar_links_categorias(bs):
+    #div da categ
+    categoria = bs.find('div', {'class':{'side_categories'}})
+    links = categoria.find_all('a')[1:] #pula o primeiro link que vai para home
+    
+    listaCategoriasComPaginacao = []
+    listaCategoriasSemPaginacao = []
+    
+    #acessando a url de cada link e vendo se tem paginação
+    #tera páginação quando houver um link com a classe next
+    for a in links:
+        url_rel = a['href']
+        url_total = URL_BASE + url_rel
+        
+        try:
+            reqCategoria = Request(
+                url=url_total,
+                headers={'User-Agent': 'Mozilla/5.0'}  
+            )
+            htmlCategoria = urlopen(reqCategoria).read()
+            time.sleep(2)
+            bsCategoria = BeautifulSoup(htmlCategoria, "html.parser")
+            
+            #verifica se existe o link next na div de paginacao
+            pageNext = bsCategoria.find('li', {'class':'next'})
+            if pageNext:
+                listaCategoriasComPaginacao.append(url_total)
+                print(f"url com paginação encontrada - {url_total}")
+            else:
+                listaCategoriasSemPaginacao.append(url_total)
+                print(f"url sem paginação encontrada - {url_total}")
+        except URLError as e:
+            print(f"erro ao processar a categoria {url_total} {e}")
+            continue
+    #encontra 3 url nao paginadas e 2 sim
+    escolhidas = sample(listaCategoriasComPaginacao, 2) + sample(listaCategoriasSemPaginacao, 3)
+   
+    return escolhidas
 
-listaMonetarios=[
-    "R$ 3.000,00",
-    "R$399,00",
-    "R$2",
-    "R$2,00",
-    "R$ 2222,00",
-    "R$ 2.222,00",
-    "R$ 2.000.000,27"
-]
+req = Request(
+    url=listaURLs[0],
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
 
-aula3.valoresMonetarios(listaMonetarios)
-
+try:
+    html = urlopen(req).read()
+    time.sleep(2)
+    
+    bs = BeautifulSoup(html, "html.parser")
+    #marca a url como já visitada
+    listaURLsVisitadas.append()
+    escolhidas = encontrar_links_categorias(bs)
+    
+    print("***lista de url escolhidas***")
+    for u in escolhidas:
+        
+        print(f"- {u}")
+    
+except URLError as e:
+    print('Servidor não encontrado')
+finally:
+    print('tudo certo')
 
